20969fb3f18f9755d62ada55332a4300fc20ce39
t-sato <t-sato@ruby-dev.jp>
2018-05-23 11:57:02 +0900

    json API

--- rails-api-server/app/controllers/rooms_controller.rb
+++ rails-api-server/app/controllers/rooms_controller.rb
@@ -46,6 +46,6 @@ class RoomsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def room_params
-      params.fetch(:room, {})
+      params.fetch(:room, {}).permit(:comment)
     end
 end
